c3645bd278783a08309d1281734baeeb19aff9f0
Joost Wagenaar <joost@blackfynn.com>
2018-06-20 21:39:36 -0400

    add compile step

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+javac -cp ../java/protobuf-java-3.5.1.jar ../java/*.java -d . -source 1.8 -target 1.8
+jar cf ../java/blackfynio.jar ../java/blackfynn/*.class
 /Applications/MATLAB_R2017b.app/bin/matlab -nodisplay -nodesktop -r "run ./createToolbox.m"
