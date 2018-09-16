93504b1b65e6a163de34db2369399d70f41a3f35
Jon Brouse <jbrouse19@gmail.com>
2018-09-16 13:26:59 -0400

    Failed build if no artifact was build

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BUILD_DIR="build"
+rm -rf ../build || true
 
 echo "Copying jars..."
 javac -cp ../java/protobuf-java-3.5.1.jar ../java/*.java -d . -source 1.8 -target 1.8
@@ -8,3 +8,5 @@ jar cf blackfynio.jar blackfynn/*.class
 
 echo -e "Running matlab build..."
 matlab -nodisplay -nodesktop -r "run ./createToolbox.m"
+
+[ -f build/blackfynn.mltbx ] || { echo "ERROR: build failed ../build/blackfynn.mltbx doesn't exist." && exit 1; }
