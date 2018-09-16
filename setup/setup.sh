6a5a49deec40f6eacf4e1299eaee922bc3091ea3
Jon Brouse <jbrouse19@gmail.com>
2018-09-16 12:48:21 -0400

    Added logging to setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,9 +2,9 @@
 
 BUILD_DIR="build"
 
+echo "Copying jars..."
 javac -cp ../java/protobuf-java-3.5.1.jar ../java/*.java -d . -source 1.8 -target 1.8
 jar cf blackfynio.jar blackfynn/*.class
 
-[ -d ${BUILD_DIR} ] || mkdir ${BUILD_DIR}
-
+echo -e "Running matlab build..."
 matlab -nodisplay -nodesktop -r "run ./createToolbox.m"
