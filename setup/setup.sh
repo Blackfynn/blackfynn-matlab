bf0ea76cc9da9f7da7c4877adb459fa8ca0f7578
Jon Brouse <jbrouse19@gmail.com>
2018-09-17 00:46:51 -0400

    setup.sh now looks at the correct dir

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 BUILD_DIR="build"
-rm -rf ${BUILD_DIR} || true
-mkdir ${BUILD_DIR}
+[ -d $BUILD_DIR ] || mkdir ${BUILD_DIR}
 
 echo "Copying jars..."
 javac -cp ../java/protobuf-java-3.5.1.jar ../java/*.java -d . -source 1.8 -target 1.8
@@ -11,4 +10,4 @@ jar cf blackfynio.jar blackfynn/*.class
 echo -e "Running matlab build..."
 matlab -nodisplay -nodesktop -r "run ./createToolbox.m"
 
-[ -f builds/blackfynn.mltbx ] || { echo "ERROR: build failed ../build/blackfynn.mltbx doesn't exist." && exit 1; }
+[ -f build/blackfynn.mltbx ] || { echo "ERROR: build failed /build/blackfynn.mltbx doesn't exist." && exit 1; }
