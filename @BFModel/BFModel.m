2c931222a85c75eb317c1733a203488f2ac1d006
Joost Wagenaar <joost@blackfynn.com>
2018-09-25 17:31:52 -0400

    added create records

--- @BFModel/BFModel.m
+++ @BFModel/BFModel.m
@@ -4,6 +4,7 @@ classdef BFModel < BFBaseModelNode
     
     properties
         nrRecords = 0           % Number of records for this model
+        props = []              % Array of property details
     end
     
     properties (Hidden)
@@ -18,6 +19,9 @@ classdef BFModel < BFBaseModelNode
             %           description, locked, created_at, updated_at]
             obj = obj@BFBaseModelNode(varargin{:});
         end
+        
+
+        
         
         function records = getall(obj, varargin)
             %GETALL Returns records of the given model
@@ -45,6 +49,54 @@ classdef BFModel < BFBaseModelNode
             resp = request.get(endPoint, params);
             records = obj.handleGetRecords(resp);
         end
+        function out = createRecord(obj, values)
+            %CREATE  Create a record for a particular model
+            %   RECORDS = CREATE(OBJ, DATA) creates a
+            %   record of type MODEL and populate the record with the
+            %   provided values where MODEL is of type BFModel. DATA is
+            %   a matlab structure with the model property names and their
+            %   values. If DATA is an array of structs, multiple objects
+            %   will be created and returned.
+            %
+            %   The property names and the property value types in the DATA
+            %   struct should match the property names and value types of
+            %   the selected model.
+            %
+            %   For example:
+            %       person = dataset.models(1);
+            %       data(1) = struct('name', 'Joe', 'age', 23);
+            %       data(2) = struct('name', 'Emily', 'age', 27);
+            %       person.createRecord(data);
+            %
+            %   See also:
+            %       BFModel.getall
+                        
+            % Create using API
+            batch_array = {};
+            for i=1: length(values)
+                curItem = values(i);
+                fields = fieldnames(curItem);
+                
+                rec_array = struct('name',{},'value',{});
+                for j=1: length(fields)
+                    rec_array(j) = struct('name',fields{j}, 'value', curItem.(fields{j}));
+                end
+                batch_array{i} = sprintf('{"values": %s }',jsonencode(rec_array)); %#ok<AGROW>
+            end
+                    
+            message = sprintf('%s,',batch_array{:});
+            message = ['[' message(1:end-1) ']'];
+
+            % Create object from response
+            uri = sprintf('%s/datasets/%s/concepts/%s/instances/batch', obj.session.concepts_host, obj.datasetId, obj.id);
+            response = obj.session.request.post(uri, message);
+            out = BFRecord.empty(length(response),0);
+            for i=1: length(response)
+                out(i) = BFRecord.createFromResponse(response(i), obj.session, obj.id, obj.datasetId);
+            end
+            
+            
+        end
     end
     
     methods (Access = protected)                            
@@ -62,6 +114,14 @@ classdef BFModel < BFBaseModelNode
                 records(i) = BFRecord.createFromResponse(resp(i), obj.session, obj.id, obj.datasetId);
             end
         end
+        function props = getProperties(obj)
+            params = {};
+            endPoint = sprintf('%s/datasets/%s/concepts/%s/properties',...
+                obj.session.concepts_host, obj.datasetId,obj.id);
+            
+            request = obj.session.request;
+            props = request.get(endPoint, params);
+        end
     end
     
     methods (Static)
@@ -76,6 +136,8 @@ classdef BFModel < BFBaseModelNode
           out.nrRecords = resp.count;
           out.nrProperties = resp.propertyCount;
           out.datasetId = datasetid;
+          out.props = out.getProperties();
+          
         end
     end
 end
