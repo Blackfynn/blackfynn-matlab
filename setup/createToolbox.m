6544cca2056dd70e42a0ca44625b888b940de818
Joost Wagenaar <joost@blackfynn.com>
2018-10-01 12:14:57 -0400

    version

--- setup/createToolbox.m
+++ setup/createToolbox.m
@@ -1,9 +1,21 @@
 % This script creates the toolbox from the project file
-projectFile = 'blackfynn.prj';
+projectFile = 'blackfynn-matlab.prj';
 outputFile = 'build/blackfynn';
 
-addpath('../');
-newVersion = Blackfynn.toolboxVersion();
+% Get version from Github tag (saved in version.txt)
+h = fopen('version.txt');
+newVersion = fscanf(h,'%s');
+[a,b]=regexp(newVersion,'(\d+\.)+\d');
+newVersion = newVersion(a:b);
+if isempty(newVersion)
+    newVersion = '1.0';
+end
+
+if newVersion(1) == '0'
+    newVersion(1) = '1';
+end
+
+% Set version and build toolbox
 matlab.addons.toolbox.toolboxVersion(projectFile, newVersion);
 matlab.addons.toolbox.packageToolbox(projectFile, outputFile)
 quit()
